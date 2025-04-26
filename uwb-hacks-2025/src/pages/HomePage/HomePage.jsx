a7c4e0932457d46060fa1e143ac1694421e8019d
Teja Dasari <ichorlifephoenix@gmail.com>
2025-04-26 14:21:25 -0700

    Settings page fleshed out.

--- uwb-hacks-2025/src/pages/HomePage/HomePage.jsx
+++ uwb-hacks-2025/src/pages/HomePage/HomePage.jsx
@@ -8,8 +8,10 @@ function HomePage() {
       <h1>Home Page</h1>
       <Link to="/litter-info/1">View Litter 123</Link>
       <br />
-        <Link to="/report-litter">Add Litter</Link>
-        <Map />
+      <Link to="/report-litter">Add Litter</Link>
+      <br />
+      <Link to="/settings">Settings</Link>
+      <Map />
     </div>
   );
 }
